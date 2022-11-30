8085ca4b30310a4c141f77a38676dacdc11723a6
Fabio Ribeiro <fcmribeiro22@gmail.com>
2022-11-30 19:15:47 +0000

    Add clearGrid function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector ('.resetbutton');
 
 
 const generateGrid =() =>{
@@ -24,6 +25,22 @@ const changeCell =() => {
 
 function setBlackColour() {
   this.style.backgroundColor = "black";}
+
+
+function clearGrid (){
+  const myHoveredCells = document.querySelectorAll('.cell');
+  for (let i = 0; i < myHoveredCells.length; i++) {
+   myHoveredCells[i].style.backgroundColor = "white";}
+ }
+
+
+
+
+
+  resetButton.addEventListener('click',clearGrid)
+
+
+
 
 
   generateGrid();
