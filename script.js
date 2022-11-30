0cdc903e2c19f7fa17c617218938e687edeb166a
Fabio Ribeiro <fcmribeiro22@gmail.com>
2022-11-30 16:08:51 +0000

    Turn generate grid and cell change into functions

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const container = document.querySelector('.container');
-const myBoard = document.createElement('div');
-myBoard.className ="board-container"
-container.appendChild(myBoard);
 
 
-
-for (let i = 0; i < 256; i++) {
+const generateGrid =() =>{
+  for (let i = 0; i < 256; i++) {
     const myCell = document.createElement('div');
     myCell.className ="cell";
-    myBoard.appendChild(myCell);
-    container.appendChild(myBoard);
-
+    container.appendChild(myCell);
+    
   }
+  
+}
+
+const changeCell =() => {
+  const myHoveredCells = document.querySelectorAll('.cell');
+  for (let i = 0; i < myHoveredCells.length; i++) {
+   myHoveredCells[i].addEventListener("mouseover", setBlackColour);
+ }
+
+
+
+}
+
+
+function setBlackColour() {
+  this.style.backgroundColor = "black";}
+
+
+  generateGrid();
+  changeCell();
