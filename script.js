3b238e4a7c40fabd79c8d46a534ed6f330ca3a76
Fabio Ribeiro <fcmribeiro22@gmail.com>
2022-12-06 15:37:50 +0000

    Add Prompt to choose Cells

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 const container = document.querySelector('.container');
-const resetButton = document.querySelector ('.resetbutton');
+const button = document.getElementById("button");
 
 
-const generateGrid =() =>{
-  for (let i = 0; i < 256; i++) {
+const generateGrid =(size) =>{
+  container.style.setProperty("--grid", size);
+  for (let i = 0; i < (size * size); i++) {
     const myCell = document.createElement('div');
-    myCell.className ="cell";
-    container.appendChild(myCell);
+      container.appendChild(myCell).className ="cell";
+      changeCell();
     
   }
-  
 }
 
+
+
+//Change on Hover
 const changeCell =() => {
   const myHoveredCells = document.querySelectorAll('.cell');
   for (let i = 0; i < myHoveredCells.length; i++) {
    myHoveredCells[i].addEventListener("mouseover", setBlackColour);
  }
-
-
-
 }
 
 
 function setBlackColour() {
-  this.style.backgroundColor = "black";}
+this.style.backgroundColor = "black";}
 
 
-function clearGrid (){
-  const myHoveredCells = document.querySelectorAll('.cell');
-  for (let i = 0; i < myHoveredCells.length; i++) {
-   myHoveredCells[i].style.backgroundColor = "white";}
- }
+generateGrid(16);
 
 
+const changesize = (size) => {
+  container.innerHTML="";
+  size= prompt ('Enter a number between 0 and 100');
+  if (size > 1 && size < 100){
+    generateGrid(size);
+  }
+  else {
+    generateGrid(16);
+    alert ('Choose only between 1 and 100 ')
+  }
 
+}
 
+button.addEventListener('click', changesize);
 
-  resetButton.addEventListener('click',clearGrid)
-
-
-
-
-
-  generateGrid();
-  changeCell();
